549116b877fb039ab7838fb4ecd31916ce924527
Sasha Dagayev <odagayev@gmail.com>
2023-04-03 22:03:01 -0400

    first commit

---
+++ cli_error_app.py
@@ -0,0 +1,65 @@
+import argparse
+import openai
+import os
+import dotenv
+import platform
+import sys
+from termcolor import colored
+
+# Load environment variables from .env file
+dotenv.load_dotenv()
+
+# Function to communicate with the ChatGPT API
+def ask_chatgpt(api_key, error_message, input=None, environment_info=None):
+    #delete the last line of environment_info for presentation purposes
+    lines = environment_info.split("\n")
+    lines_without_last = lines[:-1]
+    environment_info = "\n".join(lines_without_last)
+    message = f"""
+I ran the following command:\n{input}\n
+I received the following error message:\n{error_message}\n
+Here is everything I know about my environment:\n{environment_info}\n
+{colored('How can I fix it?', 'black', attrs=['bold'])}
+    """
+    print(message)  # Debugging line
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        api_key=api_key,
+        messages=[
+            {"role":"system", "content":"You are a very helpful technical assistant. Please keep explanations as simple as possible."},
+            {"role":"user", "content":message},
+        ]
+    )
+    formatted_message = response['choices'][0]['message']['content']
+    return formatted_message
+
+def get_environment_info():
+    environment_info = {
+        "operating_system": platform.system(),
+        "os_version": platform.release(),
+        "python_version": sys.version,
+        "environment_variables": "\n" + "\n".join([f"{k}: {v}" for k, v in os.environ.items()])
+    }
+    formatted_info = "\n".join([colored(f"{key}: {value}",'blue') for key, value in environment_info.items()])
+    return formatted_info
+
+# Main function
+def main():
+    # Define and parse CLI arguments
+    parser = argparse.ArgumentParser(description="Interpret and fix terminal errors using ChatGPT")
+    parser.add_argument("error_message", type=str, help="Error message from the terminal")
+    args = parser.parse_args()
+
+    command, error_message = args.error_message.split('|||')
+    #strip the equal api_key= from the api_key
+    api_key = os.getenv("OPENAI_API_KEY")
+    
+    # Ask ChatGPT for a solution to the error
+    solution = ask_chatgpt(api_key=api_key,error_message=colored(error_message, 'red'), input=colored(command, 'blue'), environment_info=get_environment_info())
+
+    # Display the solution to the user
+    print(colored("ChatGPT suggests the following solution:", 'green', attrs=['bold']))
+    print(colored(solution, 'black', attrs=['bold']))
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,57 @@
+# CLI Error Assistant
+
+CLI Error Assistant is a command-line utility that helps you automatically diagnose and fix errors encountered in the terminal. By leveraging the OpenAI ChatGPT API, the utility provides suggestions for fixing common errors in a user-friendly manner.
+
+## Features
+
+- Automatically captures errors in the terminal
+- Parses error messages and environment information
+- Communicates with OpenAI ChatGPT API to get error resolution suggestions
+- Displays solutions in a user-friendly and color-coded format
+
+## Prerequisites
+
+- Python 3.6 or later
+- OpenAI Python library (v0.27.0 or later)
+- `termcolor` library
+- An OpenAI API key
+
+## Installation
+
+1. Clone this repository:
+
+```
+git clone https://github.com/yourusername/cli-error-assistant.git
+```
+
+2. Install the required Python libraries:
+
+```
+pip install openai termcolor python-dotenv
+```
+
+3. Create a `.env` file in the project's root directory and add your OpenAI API key:
+
+```
+OPENAI_API_KEY=your_api_key_here
+```
+
+4. Set up the `error_wrapper.sh` script to make it accessible from the terminal:
+
+```
+ln -s /path/to/your/error_wrapper.sh /usr/local/bin/error_wrapper
+```
+
+## Usage
+
+To use the CLI Error Assistant, simply prefix any command in the terminal with `error_wrapper`. For example:
+
+```
+error_wrapper ls non_existent_directory
+```
+
+If an error occurs, the CLI Error Assistant will capture the error message and provide a solution using the ChatGPT API.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details. 
